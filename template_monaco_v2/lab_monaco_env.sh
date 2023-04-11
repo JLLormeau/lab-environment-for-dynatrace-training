9006dac612ba9b978488e617bdd6d495bf6be644
Jean-Louis Lormeau <40337213+JLLormeau@users.noreply.github.com>
2023-04-11 19:38:45 +0200

    Add files via upload

---
+++ template_monaco_v2/lab_monaco_env.sh
@@ -0,0 +1,8 @@
+#Export variables Env
+export DT_TENANT_URL=https://abcd.live.dynatrace.com
+export DT_API_TOKEN=XXXX
+
+#Export variables appli
+export Appname=easytravelxx
+export Hostname=zzzz.yyyy.cloudapp.azure.com
+export Email=myemail@email.com
