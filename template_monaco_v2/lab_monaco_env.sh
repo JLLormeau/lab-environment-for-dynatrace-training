436e6aeafc49509edf02d3e648de160f9db2d0a1
Jean-Louis Lormeau <40337213+JLLormeau@users.noreply.github.com>
2023-04-23 14:39:05 +0200

    Delete lab_monaco_env.sh

--- template_monaco_v2/lab_monaco_env.sh
+++
@@ -1,8 +0,0 @@
-#Export variables Env
-export DT_TENANT_URL=https://abcd.live.dynatrace.com
-export DT_API_TOKEN=XXXX
-
-#Export variables appli
-export Appname=easytravelxx
-export Hostname=zzzz.yyyy.cloudapp.azure.com
-export Email=myemail@email.com
