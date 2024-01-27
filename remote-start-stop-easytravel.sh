1d564963e1092374b013e4a90722c2621685a367
Jean-Louis Lormeau <40337213+JLLormeau@users.noreply.github.com>
2024-01-27 14:22:00 +0100

    Update remote-start-stop-easytravel.sh

--- remote-start-stop-easytravel.sh
+++ remote-start-stop-easytravel.sh
@@ -31,7 +31,7 @@ do
 		ssh -tt -oStrictHostKeyChecking=no 'user'$X$i'@'$DOMAIN_NAME$X$i'.'$LOCATION'.cloudapp.azure.com' '/home/dynatracelab_easytraveld/start-stop-easytravel.sh' $response &
 
 	fi
-	if [ $i -ge 5 ] || [ $i -gt 10 ] 
+	if [ $i -ge 5 ] && [ $i -lt 10 ] 
 	then
 		X='0' #from 05 to 09
 		LOCATION=$LOCATION2
@@ -41,22 +41,22 @@ do
 
 
 	fi
-	if [ $i -ge 10 ] || [ $i -gt 15 ] 
+	if [ $i -ge 10 ] && [ $i -lt 15 ] 
 	then
 		X='' #from 10 to 14
 		LOCATION=$LOCATION3
 
-		echo 'run ssh user'$X$i'@'$DOMAIN_NAME$X$i'.'$LOCATION'.cloudapp.azure.com' $response
-		ssh -tt -oStrictHostKeyChecking=no 'user'$X$i'@'$DOMAIN_NAME$X$i'.'$LOCATION'.cloudapp.azure.com' '/home/dynatracelab_easytraveld/start-stop-easytravel.sh' $response &
+		echo 'run ssh user'$i'@'$DOMAIN_NAME$i'.'$LOCATION'.cloudapp.azure.com' $response
+		ssh -tt -oStrictHostKeyChecking=no 'user'$i'@'$DOMAIN_NAME$i'.'$LOCATION'.cloudapp.azure.com' '/home/dynatracelab_easytraveld/start-stop-easytravel.sh' $response &
 
 	fi
-	if [ $i -ge 15 ] || [ $i -gt 20 ] 
+	if [ $i -ge 15 ] && [ $i -lt 20 ] 
 	then
 		X='' #from 10 to 20
 		LOCATION=$LOCATION4
 		
-		echo 'run ssh user'$X$i'@'$DOMAIN_NAME$X$i'.'$LOCATION'.cloudapp.azure.com' $response
-		ssh -tt -oStrictHostKeyChecking=no 'user'$X$i'@'$DOMAIN_NAME$X$i'.'$LOCATION'.cloudapp.azure.com' '/home/dynatracelab_easytraveld/start-stop-easytravel.sh' $response &
+		echo 'run ssh user'$i'@'$DOMAIN_NAME$i'.'$LOCATION'.cloudapp.azure.com' $response
+		ssh -tt -oStrictHostKeyChecking=no 'user'$i'@'$DOMAIN_NAME$i'.'$LOCATION'.cloudapp.azure.com' '/home/dynatracelab_easytraveld/start-stop-easytravel.sh' $response &
 
 	fi     
 	i=$(($i + 1))
