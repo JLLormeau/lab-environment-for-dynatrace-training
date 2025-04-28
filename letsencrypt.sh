f9f1e83df3d8ab86a25b2d6d5e2aeef7c98c1a6a
Jean-Louis Lormeau <40337213+JLLormeau@users.noreply.github.com>
2025-04-28 12:10:29 +0200

    Update letsencrypt.sh

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -1,11 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
-domain=`ls /var/lib/shellinabox | grep cloudapp | cut -d'-' -f2- | sed 's/.pem//'`
-user=`whoami`
+#domain=`ls /var/lib/shellinabox | grep cloudapp | cut -d'-' -f2- | sed 's/.pem//'`
+#user=`whoami`
+domain=$1
+user=$2
 echo $domain
 echo $user
-sudo /home/dynatracelab_easytraveld/start-stop-easytravel.sh stop
-sudo apt update
+#sudo /home/dynatracelab_easytraveld/start-stop-easytravel.sh stop
+#sudo apt update
 sudo snap install --classic certbot
 sleep 2
 sudo certbot certonly --standalone -d $domain --key-type rsa --quiet --agree-tos
@@ -16,6 +18,6 @@ sudo rm /var/lib/shellinabox/*.pem
 sudo mv /home/$user/cert.pem /var/lib/shellinabox/certificate-$domain.pem
 sudo chown shellinabox:shellinabox /var/lib/shellinabox/certificate-$domain.pem
 sudo chmod 600 /var/lib/shellinabox/certificate-$domain.pem
-
-sudo service shellinabox restart
-sudo service shellinabox status
+                                                                                                                                                                        
+#sudo service shellinabox restart
+#sudo service shellinabox status
